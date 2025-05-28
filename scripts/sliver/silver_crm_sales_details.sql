40b9239925f9f2deb526ec62426c0ab877a892f6
Obed Y.D. Osei-Nyansah <79172779+iamobedson@users.noreply.github.com>
2025-05-28 17:56:31 +0000

    Update silver_crm_sales_details.sql'

--- scripts/sliver/silver_crm_sales_details.sql
+++ scripts/sliver/silver_crm_sales_details.sql
@@ -18,13 +18,13 @@ sls_prd_key,
 sls_cst_id,
 CASE WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
 	 ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
-END AS sls_order_dt,
+END AS sls_order_dt,				-- Cast order Date from String to DATE variable
 CASE WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
 	 ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
-END AS sls_ship_dt,
+END AS sls_ship_dt,				-- Cast shipping Date from String to DATE variable
 CASE WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
 	 ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
-END AS sls_due_dt,
+END AS sls_due_dt,				-- Cast due Date from String to DATE variable
 CASE WHEN sls_sales IS NULL or sls_sales <=0 OR sls_sales != sls_quantity * ABS(sls_price)
 		THEN sls_quantity * ABS(sls_price)
 	ELSE sls_sales
