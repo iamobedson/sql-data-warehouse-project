5b587aca17a7ec29a7e64220c61b8776075a91fe
Obed Y.D. Osei-Nyansah <79172779+iamobedson@users.noreply.github.com>
2025-05-28 17:54:55 +0000

    Update silver_crm_sales_details.sql

--- scripts/sliver/silver_crm_sales_details.sql
+++ scripts/sliver/silver_crm_sales_details.sql
@@ -28,11 +28,11 @@ END AS sls_due_dt,
 CASE WHEN sls_sales IS NULL or sls_sales <=0 OR sls_sales != sls_quantity * ABS(sls_price)
 		THEN sls_quantity * ABS(sls_price)
 	ELSE sls_sales
-END AS sls_sales,
+END AS sls_sales, 				-- Recalculate Sales if Original Value is Mission or incorrect
 sls_quantity,
 CASE WHEN sls_price IS NULL OR sls_price <=0
 		THEN sls_sales / NULLIF(sls_quantity, 0)
 	ELSE sls_price
-END as sls_price
+END as sls_price 				-- Derive Price if the original value is invalid
 FROM bronze.crm_sales_details
 
