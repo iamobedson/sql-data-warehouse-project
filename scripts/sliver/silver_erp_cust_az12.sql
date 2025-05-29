fbf89e1970c870b1881726391b6683b5cdc3db1a
Obed Y.D. Osei-Nyansah <79172779+iamobedson@users.noreply.github.com>
2025-05-29 11:22:07 +0000

    Update silver_erp_cust_az12.sql

--- scripts/sliver/silver_erp_cust_az12.sql
+++ scripts/sliver/silver_erp_cust_az12.sql
@@ -1,5 +1,5 @@
 /*
-THis script cleans and inserts data into the ERP Cust table
+This script cleans and inserts data into the ERP Cust table
 */
 INSERT INTO silver.erp_cust_az12(
 cid,
@@ -7,15 +7,15 @@ bdate,
 gen
 )
 SELECT
-CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
+CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid)) -- Remove 'NAS' prefix if present
 	ELSE cid
 END cid,
 CASE WHEN bdate > GETDATE() THEN NULL
 	ELSE bdate
-END bdate,
+END bdate,						-- Set future birthdate to NULL
 CASE 
 	WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
 	WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
 	ELSE 'n/a'
-END gen
+END gen							-- Normalise gender values and handle unknown cases
 FROM bronze.erp_cust_az12
